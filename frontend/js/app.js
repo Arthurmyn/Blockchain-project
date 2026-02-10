ec152e6388a342a09b9b53d23df3ab0149601a0d
Neriadius <darin1980museev@gmail.com>
2026-02-10 14:52:13 +0500

    add button collect

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,7 +7,7 @@ const tokenBalanceEl = document.getElementById("tokenBalance");
 const createBtn = document.getElementById("createCampaign");
 const campaignList = document.getElementById("campaignList");
 
-const TOKEN_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const ERC20_ABI = [
   "function balanceOf(address owner) view returns (uint256)",
   "function decimals() view returns (uint8)",
@@ -169,6 +169,14 @@ async function loadCampaigns() {
         ${isClosed ? "disabled" : ""}
       >
         ${isClosed ? "Closed" : "Donate"}
+      </button>
+      <button
+        class="btn collect collect-btn"
+        data-campaign-id="${c.blockchainId}"
+        data-wallet-id="${c.walletAddress}"
+        ${isClosed ? "disabled" : ""}
+      >
+        ${isClosed ? "Closed" : "Collect"}
       </button>
     `;
     campaignList.appendChild(div);
